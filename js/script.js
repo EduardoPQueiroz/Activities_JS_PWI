9471dc9419e7110b3cfbe849a20bda986424aa52
Eduardo Pereira de Queiroz <eduardopq4@gmail.com>
2023-09-27 15:15:43 -0300

    feat - get pair number from array function.

--- js/script.js
+++ js/script.js
@@ -16,10 +16,19 @@ function Nome(param1, param2){
 //Função MAP
 //Array = [ ]
 //Objeto Produto = { foto, modelo, preço, qtd em estoque, nome, codigo }
-const numeros =  [5, 48, 65, 33, 12, 79, 8, 44]
-const dobro = []
-numeros.map( (num) => {
-    dobro.push(num*2)    
+//const numeros =  [5, 48, 65, 33, 12, 79, 8, 44]
+//const dobro = []
+//numeros.map( (num) => {
+//    dobro.push(num*2)    
+//} )
+//console.log(numeros);
+//console.log(dobro);
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+const par = []
+array.map( (x) => {
+    if(x % 2 === 0){
+    par.push(x)
+    }
 } )
-console.log(numeros);
-console.log(dobro);
+console.log(array);
+console.log(par);
