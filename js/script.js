3e435d4a5f8ae928da8acf108943ab04275fc601
Eduardo Pereira de Queiroz <eduardopq4@gmail.com>
2023-09-27 15:22:43 -0300

    Fixing details

--- js/script.js
+++ js/script.js
@@ -16,13 +16,15 @@ function Nome(param1, param2){
 //Função MAP
 //Array = [ ]
 //Objeto Produto = { foto, modelo, preço, qtd em estoque, nome, codigo }
-//const numeros =  [5, 48, 65, 33, 12, 79, 8, 44]
-//const dobro = []
-//numeros.map( (num) => {
-//    dobro.push(num*2)    
-//} )
-//console.log(numeros);
-//console.log(dobro);
+//Dobra numeros do Array
+const numeros =  [5, 48, 65, 33, 12, 79, 8, 44]
+const dobro = []
+numeros.map( (num) => {
+    dobro.push(num*2)    
+} )
+console.log(numeros);
+console.log(dobro);
+//Pega os numeros pares do array
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 const par = []
 array.map( (x) => {
